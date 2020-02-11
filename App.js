bce51c99a972bbd8a22bfb0189c9b68f024c2e11
aluno <leonardopessoapm@gmail.com>
2020-02-11 17:41:00 -0300

    my first react web app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>lalallalalal</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    fontSize: '245px',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
